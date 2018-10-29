09a7610b6ecba596c5db21013ea53a33a59d1047
Jan Hansen <admin@TA-828-01>
2018-10-29 15:52:19 +0100

    kursus

---
+++ Tal_opgave/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tal_opgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int heltal = 10;
+
+            heltal++;
+
+            heltal--;
+
+            heltal += 20;
+
+            Console.WriteLine(heltal);
+
+            double kommatal = 12.5;
+
+            kommatal++;
+
+            kommatal--;
+
+            kommatal *= 20;
+
+            Console.WriteLine(kommatal);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Variable_Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a;
+            int b = 10;
+
+            bool c;
+            bool d = false;
+
+            int e, f, g;
+
+            double y = 12.1;
+
+            double x = 12;
+
+
+            a = x + b;
+
+            Console.WriteLine(a);
+
+            a++;
+
+
+            Console.WriteLine(a);
+
+            a++;
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(d);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
