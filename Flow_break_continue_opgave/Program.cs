841df7850c4687ae6e27291f14d8b9915db092a1
Jan Hansen <jah@sv.dk>
2018-11-06 19:48:40 +0100

    lavet opgave

---
+++ Flow_break_continue_opgave/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flow_break_continue_opgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 19; i++)
+            {
+                if (i % 3 != 0)
+                    continue;
+
+
+
+                if (i > 15)
+                    break;
+
+
+
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- Metoder/Program.cs
+++ Metoder/Program.cs
@@ -11,7 +11,35 @@ namespace Metoder
         static void Main(string[] args)
         {
 
+            int res = LægSammen(5, 2);
+            Console.WriteLine(res);
+            double res2 = BeregnAreal(5);
+            Console.WriteLine(res2);
+            Udskriv("dette er en test");
 
+
+            // Hold console åben ved debug 
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+        static int LægSammen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double BeregnAreal(double radius)
+        {
+            return (radius * radius) * Math.PI;
+
+        }
+
+        static void Udskriv(string text)
+        {
+            Console.WriteLine(text);
         }
     }
 
